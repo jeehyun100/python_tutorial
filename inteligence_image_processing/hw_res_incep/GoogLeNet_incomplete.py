328f7828e5b12d1216672b797be297dcedd4f8fd
intwis100 <intwis100@naver.com>
2019-11-09 12:33:39 +0900

    ADD: res_inception

--- inteligence_image_processing/hw_res_incep/GoogLeNet_incomplete.py
+++ inteligence_image_processing/hw_res_incep/GoogLeNet_incomplete.py
@@ -20,38 +20,23 @@ print("Training Set:   {} samples".format(len(X_train)))
 print("Validation Set: {} samples".format(len(X_valid)))
 print("Test Set:       {} samples".format(len(X_test)))
 
+
 def conv2d(input, filters, kernel_size, strides=1, activation=tf.nn.relu, padding='SAME', name='conv'):
     with tf.variable_scope(name, reuse=False):
         out = tf.layers.conv2d(input, filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
                          activation=activation, name=name)
     return out
 
+
 def dense(input, unit, activation=tf.nn.relu, name='dense'):
     with tf.variable_scope(name, reuse=False):
         out = tf.layers.dense(input, unit, activation=activation, name=name)
     return out
 
+
 def max_pool(input, k, s, name='pool'):
     out = tf.nn.max_pool(input, ksize=[1, k, k, 1], strides=[1, s, s, 1], padding='SAME', name=name)
     return out
-
-#
-# @add_arg_scope
-# def inception_layer(conv_11_size, conv_33_reduce_size, conv_33_size,
-#                     conv_55_reduce_size, conv_55_size, pool_size,
-#                     layer_dict, inputs=None,
-#                     bn=False, wd=0, init_w=None,
-#                     pretrained_dict=None, trainable=True, is_training=True,
-#                     name='inception'):
-#
-# def max_pool(x, name, filter_height = 3, filter_width = 3,
-# 	stride = 2, padding = 'VALID'):
-#
-# 	"""Create a max pooling layer."""
-#
-# 	return tf.nn.max_pool(x, ksize = [1, filter_height, filter_width, 1],
-# 		strides = [1, stride, stride, 1], padding = padding, name = name)
-
 
 
 def inception_block(x, conv11_size, conv33_rsize, conv33_size, conv55_rsize, conv55_size, pool_size, name='incept'):
@@ -59,11 +44,8 @@ def inception_block(x, conv11_size, conv33_rsize, conv33_size, conv55_rsize, con
     ### Use conv2d and max_pool function
 
     with tf.variable_scope(name) as scope:
-        #def conv2d(input, filters, kernel_size, strides=1, activation=tf.nn.relu, padding='SAME', name='conv'):
         conv_1 = conv2d(x, conv11_size, kernel_size=1, name='{}_1x1'.format(name))
 
-        # conv_3_reduce = conv_layer(x, filter_height=1, filter_width=1,
-        #                            num_filters=conv_3_reduce_size, name='{}_3x3_reduce'.format(name))
         conv_3_reduce = conv2d(x, conv33_rsize, kernel_size=1, name='{}_3x3_reduce'.format(name))
 
         conv_3 = conv2d(conv_3_reduce, conv33_size, kernel_size=3, name='{}_3x3'.format(name))
@@ -73,7 +55,6 @@ def inception_block(x, conv11_size, conv33_rsize, conv33_size, conv55_rsize, con
         conv_5 = conv2d(conv_5_reduce, conv55_size, kernel_size = 5, name='{}_5x5'.format(name))
 
         pool = max_pool(x, 3, 1, name='{}_pool'.format(name))
-        #pool = max_pool(x, stride =1, padding = 'SAME', name = '{}_pool'.format(name))
 
         pool_proj = conv2d(pool, pool_size, kernel_size=1, name='{}_pool_proj'.format(name))
 
@@ -81,39 +62,36 @@ def inception_block(x, conv11_size, conv33_rsize, conv33_size, conv55_rsize, con
 
     return out
 
+
 def googlenet(x):
     ### Assignment ###
     ### Use cpmv2d, max_pool, inception_block, dens and dropout function
-    # 3, filter_width = 3,stride = 2
     conv1 = conv2d(x, 64, kernel_size=5, strides=1, name='conv1')
     pool1 = max_pool(conv1, 3, 2, name='pool1')
 
     inception2a = inception_block(pool1, 64, 96, 128, 16, 32, 32, name='inception1a')
     inception2b = inception_block(inception2a, 128, 128, 192, 32, 96, 64, name='inception1b')
     pool2 = max_pool(inception2b,3, 2, name='pool2')
+
     inception3a = inception_block(pool2, 192, 96, 208, 16, 48, 64, name='inception3a')
     inception3b = inception_block(inception3a, 160, 112, 224, 24, 64, 64, name='inception3b')
+
     gap = tf.nn.avg_pool(inception3b, ksize=[1, 6, 6, 1], strides=[1, 1, 1, 1], padding='VALID', name='gap')
     gap_dropout = tf.nn.dropout(gap, keep_prob=keep_prob)
+
     flatten = tf.layers.flatten(gap_dropout)
     dense2 = dense(flatten, 10, name='fc4')
 
     return dense2
 
-
-
-epochs = 100
+epochs = 10
 learning_rate = 0.00001
 batch_size = 64
 dropout = 0.8
 
-
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, 28, 28, 1])
 y = tf.placeholder(tf.int32, [None, 10])
-#hold_prob = tf.placeholder(tf.float32)
-# y_one_hot = tf.squeeze(tf.one_hot(y, 10), axis=1)
-
 keep_prob = tf.placeholder(tf.float32, [])  # dropout (keep probability)
 x_resize = tf.image.resize_images(x, [64, 64])
 
@@ -156,6 +134,8 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
         loss_total /= (len(X_train) / batch_size)
         print("epoch: " + str(i) + ", Training Loss= " + "{:.4f}".format(
             loss_total) + ", Training Accuracy= " + "{:.4f}".format(acc_total))
-
+    test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images[: 1000],
+                                             y: mnist.test.labels[: 1000], keep_prob: 1.0})
+    print("test_Accuracy : {0}".format(test_accuracy))
     print("Optimization Finished!")
     saver.save(sess, './goolgenet')
--- inteligence_image_processing/hw_res_incep/ResNet_incomplete.py
+++ inteligence_image_processing/hw_res_incep/ResNet_incomplete.py
@@ -7,13 +7,6 @@ import numpy as np
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-
-# (X_train, y_train), (X_test, y_test) = load_data()
-
-# weight_init = tf.layers.variance_scaling_initializer()
-# weight_regularizer = tf.layers.l2_regularizer(0.0001)
-
-
 mnist = input_data.read_data_sets("MNIST_data/", reshape=False, one_hot=True)
 X_train, y_train = mnist.train.images, mnist.train.labels
 X_valid, y_valid = mnist.validation.images, mnist.validation.labels
@@ -25,45 +18,23 @@ print("Validation Set: {} samples".format(len(X_valid)))
 print("Test Set:       {} samples".format(len(X_test)))
 
 
-
 def conv2d(input, filters, kernel_size, strides=1, activation=tf.nn.relu, padding='SAME', name='conv'):
     with tf.variable_scope(name, reuse=False):
         out = tf.layers.conv2d(input, filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
                          activation=activation)
     return out
 
+
 def dense(input, unit, activation=tf.nn.relu, name='dense'):
     with tf.variable_scope(name, reuse=False):
-        out = tf.layers.flatten(input)
-        out = tf.layers.dense(out, unit, activation=activation, name=name)
+        out = tf.layers.dense(input, unit, activation=activation, name=name)
     return out
+
 
 def max_pool(input, k, s, name='pool'):
     out = tf.nn.max_pool(input, ksize=[1, k, k, 1], strides=[1, s, s, 1], padding='SAME', name=name)
     return out
 
-# def resblock(x_init, channels, is_training=True, use_bias=True, downsample=False, scope='resblock') :
-#     with tf.variable_scope(scope) :
-#
-#         x = batch_norm(x_init, is_training, scope='batch_norm_0')
-#         x = relu(x)
-#
-#         #def conv2d(input, filters, kernel_size, strides=1, activation=tf.nn.relu, padding='SAME', name='conv'):
-#
-#         if downsample :
-#             x = conv2d(x, channels, kernel=3, stride=2, name='conv_0')
-#             x_init = conv2d(x_init, channels, kernel=1, stride=2, name='conv_0')
-#
-#         else :
-#             x = conv2d(x, channels, kernel=3, stride=1, name='conv_0')
-#
-#         x = batch_norm(x, is_training, scope='batch_norm_1')
-#         x = relu(x)
-#         x = conv2d(x, channels, kernel=3, stride=1, name='conv_1')
-#
-#
-#
-#         return x + x_init
 
 def res_block(x, filter_num, kernel_size, strides, name='res_block', is_training=True, downsample=False, scope='resblock' ):
 
@@ -73,8 +44,8 @@ def res_block(x, filter_num, kernel_size, strides, name='res_block', is_training
         res_x = relu(res_x)
 
         if downsample :
-            res_x = conv2d(res_x, filter_num, kernel_size, strides=strides, name='conv_0')
-            x = conv2d(res_x, filter_num, kernel=1, stride=2, name='conv_init')
+            res_x = conv2d(res_x, filter_num, kernel_size=3, strides=2, name='conv_0')
+            x = conv2d(x, filter_num, kernel_size=1, strides=2, name='conv_init')
 
         else :
             res_x = conv2d(res_x, filter_num, kernel_size, strides=strides, name='conv_0')
@@ -85,13 +56,6 @@ def res_block(x, filter_num, kernel_size, strides, name='res_block', is_training
 
         return res_x + x
 
-#
-# def fully_conneted(x, units, use_bias=True, scope='fully_0'):
-#     with tf.variable_scope(scope):
-#         x = flatten(x)
-#         x = tf.layers.dense(x, units=units, kernel_initializer=weight_init, kernel_regularizer=weight_regularizer, use_bias=use_bias)
-#
-#         return x
 
 def get_residual_layer(res_n) :
     x = []
@@ -99,72 +63,20 @@ def get_residual_layer(res_n) :
     if res_n == 18 :
         x = [2, 2, 2, 2]
 
-    if res_n == 34 :
-        x = [3, 4, 6, 3]
-
-    if res_n == 50 :
-        x = [3, 4, 6, 3]
-
-    if res_n == 101 :
-        x = [3, 4, 23, 3]
-
-    if res_n == 152 :
-        x = [3, 8, 36, 3]
-
     return x
 
-##################################################################################
-# Normalization function
-##################################################################################
 
 def batch_norm(x, is_training=True, scope='batch_norm'):
-    # return tf_contrib.layers.batch_norm(x,
-    #                                     decay=0.9, epsilon=1e-05,
-    #                                     center=True, scale=True, updates_collections=None,
-    #                                     is_training=is_training, scope=scope)
-    # return tf.nn.batch_normalization(x,
-    # mean,
-    # variance,
-    # offset,
-    # scale,
-    # variance_epsilon,
-    # name=None
-    #def batch_norm(x, n_out, phase_train):
-    """
-    Batch normalization on convolutional maps.
-    Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
-    Args:
-        x:           Tensor, 4D BHWD input maps
-        n_out:       integer, depth of input maps
-        phase_train: boolean tf.Varialbe, true indicates training phase
-        scope:       string, variable scope
-    Return:
-        normed:      batch-normalized maps
-    """
-    #with tf.variable_scope('bn'):
-        # beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
-        #                    name='beta', trainable=True)
-        # gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
-        #                     name='gamma', trainable=True)
-        # batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
-        # ema = tf.train.ExponentialMovingAverage(decay=0.5)
-        #
-        # def mean_var_with_update():
-        #     ema_apply_op = ema.apply([batch_mean, batch_var])
-        #     with tf.control_dependencies([ema_apply_op]):
-        #         return tf.identity(batch_mean), tf.identity(batch_var)
-        #
-        # mean, var = tf.cond(phase_train,
-        #                     mean_var_with_update,
-        #                     lambda: (ema.average(batch_mean), ema.average(batch_var)))
     with tf.variable_scope(scope):
         normed = tf.layers.batch_normalization(x,
                                             training=is_training)
     return normed
 
+
 def global_avg_pooling(x):
     gap = tf.reduce_mean(x, axis=[1, 2], keepdims=True)
     return gap
+
 
 def relu(x):
     return tf.nn.relu(x)
@@ -175,11 +87,6 @@ def res_net(x, is_training=True, reuse = False):
     ### Use conv2d, res_block, max_pool, dense and other function
 
     with tf.variable_scope("network", reuse=reuse):
-
-        # if self.res_n < 50:
-        #     residual_block = resblock
-        # else:
-        #     residual_block = bottle_resblock
         res_n = 18
         residual_list = get_residual_layer(res_n)
 
@@ -188,73 +95,49 @@ def res_net(x, is_training=True, reuse = False):
         strides = 1
         label_dim = 10
 
-        #conv2d(res_x, filter_num, kernel_size, strides=strides, name='conv_0')
         x = conv2d(x, 32, 3, strides=1, name='conv')
-
-        #res_block(x, filter_num, kernel_size, strides, name='res_block', is_training=True, downsample=False, scope='resblock' ):
-    ### Assignment ###
 
         for i in range(residual_list[0]):
             x = res_block(x, filter_num = filter_num, kernel_size = kernel_size, strides = strides, is_training=is_training, scope='resblock0_' + str(i))
 
+        x = res_block(x, filter_num=filter_num * 2, kernel_size = kernel_size, strides = strides, is_training=is_training, downsample=True, scope='resblock1_0')
+
+        for i in range(1, residual_list[1]):
+            x = res_block(x, filter_num=filter_num * 2, kernel_size = kernel_size, strides = strides, is_training=is_training, downsample=False,
+                               scope='resblock1_' + str(i))
+
+
+        x = res_block(x, filter_num=filter_num * 4, kernel_size = kernel_size, strides = strides, is_training=is_training, downsample=True, scope='resblock2_0')
+
+        for i in range(1, residual_list[2]):
+            x = res_block(x, filter_num=filter_num * 4, kernel_size = kernel_size, strides = strides, is_training=is_training, downsample=False,
+                               scope='resblock2_' + str(i))
+
+
+        x = res_block(x, filter_num=filter_num * 8, kernel_size = kernel_size, strides = strides, is_training=is_training, downsample=True, scope='resblock_3_0')
+
+        for i in range(1, residual_list[3]):
+            x = res_block(x, filter_num=filter_num * 8, kernel_size = kernel_size, strides = strides, is_training=is_training, downsample=False,
+                               scope='resblock_3_' + str(i))
+
         x = batch_norm(x, is_training=is_training, scope='batch_norm')
-        #x = relu(x)
 
         x = global_avg_pooling(x)
+        x = tf.nn.dropout(x, keep_prob=keep_prob)
+        x = tf.layers.flatten(x)
         x = dense(x, label_dim, name='logit')
 
     return x
 
 
-
-
-
-def get_residual_layer(res_n) :
+def get_residual_layer(res_n):
     x = []
-
-    if res_n == 18 :
+    if res_n == 18:
         x = [2, 2, 2, 2]
-
-    if res_n == 34 :
-        x = [3, 4, 6, 3]
-
-    if res_n == 50 :
-        x = [3, 4, 6, 3]
-
-    if res_n == 101 :
-        x = [3, 4, 23, 3]
-
-    if res_n == 152 :
-        x = [3, 8, 36, 3]
-
     return x
 
 
-
-# ##################################################################################
-# # Sampling
-# ##################################################################################
-#
-# def flatten(x) :
-#     return tf.layers.flatten(x)
-#
-#
-#
-# def avg_pooling(x) :
-#     return tf.layers.average_pooling2d(x, pool_size=2, strides=2, padding='SAME')
-#
-# ##################################################################################
-# # Activation function
-# ##################################################################################
-#
-#
-# def relu(x):
-#     return tf.nn.relu(x)
-
-
-
-
-epochs = 100
+epochs = 10
 learning_rate = 0.001
 batch_size = 500
 dropout = 0.8
@@ -263,9 +146,6 @@ dropout = 0.8
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, 28, 28, 1])
 y = tf.placeholder(tf.int32, [None, 10])
-# y_one_hot = tf.squeeze(tf.one_hot(y, 10),axis=1)
-#phase_train = tf.placeholder(tf.bool, name='phase_train')
-
 
 keep_prob = tf.placeholder(tf.float32, [])  # dropout (keep probability)
 x_resize = tf.image.resize_images(x, [64, 64])
@@ -282,16 +162,6 @@ optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 correct_pred = tf.equal(tf.argmax(pred, -1), tf.argmax(y, -1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-
-# Define loss and optimizer
-test_cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=test_pred, labels=y))
-#optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-# Evaluate model
-test_correct_pred = tf.equal(tf.argmax(test_pred, -1), tf.argmax(y, -1))
-test_accuracy = tf.reduce_mean(tf.cast(test_correct_pred, tf.float32))
-
-
 # Initializing the variables
 init = tf.initialize_all_variables()
 
@@ -305,8 +175,6 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
         s = np.random.permutation(len(X_train))
         X_train = X_train[s]
         y_train = y_train[s]
-        #64.28.28.1
-        #64,10
         loss_total, acc_total = 0, 0
         for offset in range(0, len(X_train), batch_size):
             end = offset + batch_size
@@ -325,5 +193,9 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_plac
         print("epoch: " + str(i) + ", Training Loss= " + "{:.4f}".format(
             loss_total) + ", Training Accuracy= " + "{:.4f}".format(acc_total))
 
+    test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images[: 1000],
+                                             y: mnist.test.labels[: 1000], keep_prob: 1.0})
+
+    print("test_Accuracy : {0}".format(test_accuracy))
     print("Optimization Finished!")
     saver.save(sess, './resnet')
